c4e11732d7b033897df4e52b7f35ad3a5444fc61
Olga Smith <olgasales@gmail.com>
2019-06-27 10:01:05 -0400

    changed display results from rows into flex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import _ from 'lodash';
+// import './resources/css/main.css';
 
 class App extends Component {
 
@@ -17,30 +18,47 @@ class App extends Component {
     let authors = this.state.authorNames.split(",");
     let urls = [];
     let promises = [];
-    authors.map(name =>{
-      urls.push(`http://methodeabcom.staging.acml.com/sites/api_service/Insights/GetAuthorArticles?authorName=${encodeURIComponent(name)}&numRecords=10&segment=56`);
-    });
+    //localhost
     // authors.map(name =>{
-    //   urls.push(`/sites/api_service/Insights/GetAuthorArticles?authorName=${encodeURIComponent(name)}&numRecords=10&segment=56`);
+    //   urls.push(`http://methodeabcom.staging.acml.com/sites/api_service/Insights/GetAuthorArticles?authorName=${encodeURIComponent(name)}&numRecords=10&segment=56`);
     // });
+
+    //prod
+    authors.map(name =>{
+      urls.push(`/sites/api_service/Insights/GetAuthorArticles?authorName=${encodeURIComponent(name)}&numRecords=10&segment=56`);
+    });
     
     //create promises
     for (let i=0; i < urls.length; i++){
       promises.push(axios.get(urls[i])); 
     }
 
+    //a promise to pull records by category. Add to the array of promises
+    //prod
+    promises.push(axios.get('/sites/api_service/Insights/GetTopicsResults?numRecords=5&insightCategory=Economic+Perspectives'));
+    //localhost
+    //promises.push(axios.get('http://methodeabcom.staging.acml.com/sites/api_service/Insights/GetTopicsResults?numRecords=5&insightCategory=Economic+Perspectives'));
+    console.log(promises);
+
     var Promise = require("es6-promise");
     Promise.polyfill();
     //wait for all promises to resolve and get the results
     Promise.all(promises).then(function(values) {
-      var arr1 = values[0].data.InsightsResults.Records;
-      var arr2 = values[1].data.InsightsResults.Records;
-      var joined = arr1.concat(arr2);
+      console.log(`values from inside promises: ${values[0].data.InsightsResults.Records}`);
+      // var arr1 = values[0].data.InsightsResults.Records;
 
+      var joined = [];
+      for (let i=0; i < values.length; i++){
+        joined.push(values[i].data.InsightsResults.Records);
+      }
+      console.log(`joined: ${joined}`);
+      //concatinate all results into one array. First parameter in apply function says to concat to empty array
+      joined = Array.prototype.concat.apply([], joined);
+      console.log(joined);
       //remove duplicates
       var merged = _.uniqWith(joined, _.isEqual);
       //var merged = self.removeDuplicateObjects(joined, 'GUID')
-
+      console.log(merged);
       //sort by date
       merged = self.sortByKey(merged, 'PublishedDateObj');
       merged = merged.reverse();
@@ -75,11 +93,11 @@ class App extends Component {
     return newArray;
   }
 
-  //display posts from requested start and end index, ex. display post from 0 to 3
-  displayResults(start, end){
-    return this.state.insightsData.slice(start,end).map(function(item){
+  //display posts from requested start and end index, ex. display posts from 0 to 6
+  displayResults(){
+    return this.state.insightsData.slice(0,6).map(function(item){
         return  (
-          <div className="col--12 col--sm-6 col--md-4 col--lg-4" key={item.GUID}>
+          <div className="fcol--1 fcol--sm-2 fcol--md-2 fcol--lg-3" key={item.GUID}>
               <a href={item.Url} target="_self">
                 {item.ContentTypes == "Video" ? <div className="abl--video-img"><img className="contained" src={item.HeroImage !=="" ? `http://www.alliancebernstein.com${item.HeroImage}` : `http://www.alliancebernstein.com${item.Tile1Image}`} alt="" /><div className="abl--video-icon"></div></div> : <img className="contained" src={item.HeroImage !=="" ? `http://www.alliancebernstein.com${item.HeroImage}` : `http://www.alliancebernstein.com${item.Tile1Image}`} alt="" /> }
                 <h3>{item.Headline}</h3>
@@ -91,29 +109,17 @@ class App extends Component {
     })
   }
 
-  //display 3 posts in each row
-  displayThreePerRow(numRows){
-    var data = [];
-    var start = 0;
-    for (let i =0; i < numRows; i++){      
-       data.push( 
-       <div className="abl--row insights--3-row abl--padded-b" key={`row${i}`}>
-          {this.displayResults(start, start+3)}
-        </div>
-       );
-       start = start+3;
-    }
-    return data;
-  }
 
   render() {
       return (
         <div className="col--12 col--sm-12 col--md-10 col--md-offset-1">
           <div className="relative">
               <h2 className="abl--padded-b">Insights</h2>
-              <a href="javascript:void(0)" className="abl--button abl--button-square-teal-border btn-pull-right">See All Insights</a>
+              <a href="/financial-intermediary/uk/insights.htm" className="abl--button abl--button-square-teal-border btn-pull-right">See All Insights</a>
           </div>
-          {this.displayThreePerRow(2)}
+          <div className="abl--flex insights--3-row abl--padded-b">
+            {this.displayResults()}
+          </div>
         </div>
             
       );
